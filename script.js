7237db09eb4d358631d37f729de786a06431673e
Luke Martin <ljmart02@gmail.com>
2020-12-09 19:00:48 -0700

    local storage updates

--- script.js
+++ script.js
@@ -1,6 +1,67 @@
 src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
 var APIkey = "9e0d8e95fda39b3fbd21fb0a7505ef69";
 const now = moment();
+var searchInput = $("#city-input");
+var searchForm = $("#search-form");
+var submitButton = $("find-city");
+var cityList = $("#cityList");
+
+var searchedCities = [];
+
+init();
+
+function renderCities() {
+    // Render a new li for each city entered
+    for (var i = 0; i < searchedCities.length; i++) {
+        var newCity = searchedCities[i];
+
+        var li = document.createElement("li");
+        li.textContent = newCity;
+        li.setAttribute("data-index", i);
+
+        cityList.appendChild(li);
+    }
+}
+
+function init() {
+    // Get stored cities from localStorage
+    // Parsing the JSON string to an object
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+    if (storedCities !== null) {
+        searchedCities = storedCities;
+    }
+    // Render cities to the DOM
+    renderCities();
+}
+
+function storeCities() {
+    // Stringify and set "cities" key in localStorage to searchCities array
+    localStorage.setItem("cities", JSON.stringify(searchedCities));
+}
+
+// When Form is submitted...
+searchForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var cityText = searchInput.value.trim();
+
+    // Return from function if blank
+    if (cityText === "") {
+        return;
+    }
+
+    // add new cityText to Search array
+    searchedCities.push(cityText);
+    searchInput.value = "";
+
+    storeCities();
+    renderCities();
+});
+
+
+
+
 
 // Search bar accepts input and retrieves the proper API
 $("#find-city").on("click", function (event) {
@@ -22,10 +83,10 @@ $("#find-city").on("click", function (event) {
 
             // Append the data for city, date, temperature, Humidity, Wind Speed to the Jumbotron
             var todayIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
-            
-            
-            
-            $(".info").text(response.name) +$(".current-day").text(now.format("dddd MMMM Do"));
+
+
+
+            $(".info").text(response.name) + $(".current-day").text(now.format("dddd MMMM Do"));
             $(".info").append(todayIcon);
             var tempF = (response.main.temp - 273.15) * 1.80 + 32;
             $(".lead-temperature").text("Temperature: " + tempF.toFixed(2));
@@ -62,7 +123,7 @@ $("#find-city").on("click", function (event) {
                     $(".card1-Date").text(oneDayForward.format('dddd MMMM DD'))
                     var dayTwoIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + forecastResponse.list[4].weather[0].icon + ".png");
                     $(".card1-Date").append(dayTwoIcon);
-                    var forecastTemp=(forecastResponse.list[4].main.temp- 273.15) * 1.80 + 32;
+                    var forecastTemp = (forecastResponse.list[4].main.temp - 273.15) * 1.80 + 32;
                     $(".card1-Temperature").text("Temperature: " + forecastTemp.toFixed(2));
                     $(".card1-Humidity").text("Humidity: " + forecastResponse.list[4].main.humidity);
 
@@ -70,7 +131,7 @@ $("#find-city").on("click", function (event) {
                     $(".card2-Date").text(twoDayForward.format('dddd MMMM DD'))
                     var dayThreeIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + forecastResponse.list[12].weather[0].icon + ".png");
                     $(".card2-Date").append(dayThreeIcon);
-                    var forecastTemp2=(forecastResponse.list[12].main.temp- 273.15) * 1.80 + 32;
+                    var forecastTemp2 = (forecastResponse.list[12].main.temp - 273.15) * 1.80 + 32;
                     $(".card2-Temperature").text("Temperature: " + forecastTemp2.toFixed(2));
                     $(".card2-Humidity").text("Humidity: " + forecastResponse.list[12].main.humidity);
 
@@ -78,7 +139,7 @@ $("#find-city").on("click", function (event) {
                     $(".card3-Date").text(threeDayForward.format('dddd MMMM DD'))
                     var dayFourIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + forecastResponse.list[20].weather[0].icon + ".png");
                     $(".card3-Date").append(dayFourIcon);
-                    var forecastTemp3=(forecastResponse.list[20].main.temp- 273.15) * 1.80 + 32;
+                    var forecastTemp3 = (forecastResponse.list[20].main.temp - 273.15) * 1.80 + 32;
                     $(".card3-Temperature").text("Temperature: " + forecastTemp3.toFixed(2));
                     $(".card3-Humidity").text("Humidity: " + forecastResponse.list[20].main.humidity);
 
@@ -86,7 +147,7 @@ $("#find-city").on("click", function (event) {
                     $(".card4-Date").text(fourDayForward.format('dddd MMMM DD'))
                     var dayFiveIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + forecastResponse.list[28].weather[0].icon + ".png");
                     $(".card4-Date").append(dayFiveIcon);
-                    var forecastTemp4=(forecastResponse.list[28].main.temp- 273.15) * 1.80 + 32;
+                    var forecastTemp4 = (forecastResponse.list[28].main.temp - 273.15) * 1.80 + 32;
                     $(".card4-Temperature").text("Temperature: " + forecastTemp4.toFixed(2));
                     $(".card4-Humidity").text("Humidity: " + forecastResponse.list[28].main.humidity);
 
@@ -94,7 +155,7 @@ $("#find-city").on("click", function (event) {
                     $(".card5-Date").text(fiveDayForward.format('dddd MMMM DD'))
                     var daySixIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + forecastResponse.list[36].weather[0].icon + ".png");
                     $(".card5-Date").append(daySixIcon);
-                    var forecastTemp5=(forecastResponse.list[36].main.temp- 273.15) * 1.80 + 32;
+                    var forecastTemp5 = (forecastResponse.list[36].main.temp - 273.15) * 1.80 + 32;
                     $(".card5-Temperature").text("Temperature: " + forecastTemp5.toFixed(2));
                     $(".card5-Humidity").text("Humidity: " + forecastResponse.list[36].main.humidity);
 
